3140a8a71229b3fd73e758fb1db5756326be7b4b
ChNikS <cherednik.nickita@yandex.ru>
2018-05-02 18:49:17 +0300

    baseline sketch

--- src/main/java/com/jetbrains/python/inspection/PyConstantExpression.java
+++ src/main/java/com/jetbrains/python/inspection/PyConstantExpression.java
@@ -2,7 +2,9 @@ package com.jetbrains.python.inspection;
 
 import com.intellij.codeInspection.LocalInspectionToolSession;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiWhiteSpace;
 import com.jetbrains.python.inspections.PyInspection;
 import com.jetbrains.python.inspections.PyInspectionVisitor;
 import com.jetbrains.python.psi.*;
@@ -38,6 +40,44 @@ public class PyConstantExpression extends PyInspection {
             if (condition instanceof PyBoolLiteralExpression) {
                 registerProblem(condition, "The condition is always " + ((PyBoolLiteralExpression) condition).getValue());
             }
+
+            //baseline handler
+            if(condition instanceof PyBinaryExpression) {
+                PsiElement firstPsiElement = condition.getFirstChild();
+                Integer firstElement = handleConditionIntElement(firstPsiElement);
+                Integer lastElement = handleConditionIntElement(condition.getLastChild());
+                handleConditionResult(firstElement, lastElement, condition);
+            }
+        }
+
+
+        private Integer handleConditionIntElement(PsiElement element) {
+            if(element instanceof PyNumericLiteralExpression) {
+                //can use big int instead
+                return ((PyNumericLiteralExpression) element).getBigIntegerValue().intValue();
+            }
+           return null;
+        }
+
+        private void handleConditionResult(Integer firstElement, Integer lastElement, PsiElement condition) {
+            if(firstElement != null && lastElement != null) {
+                String operation = ((PyBinaryExpression)condition).getOperator().toString();
+
+                if(operation.equals("Py:GT")) {
+                    registerProblem(condition, "The condition is always " + (lastElement > firstElement));
+                }
+                if(operation.equals("Py:LT")) {
+                    registerProblem(condition, "The condition is always " + (lastElement < firstElement));
+                }
+                if(operation.equals("Py:EQEQ")) {
+                    registerProblem(condition, "The condition is always " + (lastElement == firstElement));
+                }
+                if(operation.equals("Py:NE")) {
+                    registerProblem(condition, "The condition is always " + (lastElement != firstElement));
+                }
+
+            }
+
         }
     }
 }
